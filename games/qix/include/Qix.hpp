c47dc593dcedbc328bc249c9a3126b39f7bdc8a8
pierre.hamel <pierre1754@outlook.com>
2022-04-01 23:20:51 +0200

    ADDED: init Qix game

---
+++ games/qix/include/Qix.hpp
@@ -0,0 +1,26 @@
+/*
+** EPITECH PROJECT, 2022
+** Include
+** File description:
+** qix
+*/
+
+#ifndef QIX_HPP
+#define QIX_HPP
+
+#include "IGame.hpp"
+
+#include <map>
+#include <functional>
+
+namespace arc
+{
+    class Qix : public IGame {
+        public:
+
+        private:
+
+    };
+}
+
+#endif // QIX_HPP
---
+++ games/qix/src/Qix.cpp
@@ -0,0 +1,8 @@
+/*
+** EPITECH PROJECT, 2022
+** Arcade
+** File description:
+** Qix
+*/
+
+#include "Qix.hpp"
